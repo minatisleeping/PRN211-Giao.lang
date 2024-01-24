e2b93b47b41d7d0b2a0a14a867f297270184044b
minatisleeping <minatt2002@gmail.com>
2024-01-24 20:16:55 +0700

    Slot7 version2

---
+++ Session03-OOP/SchoolManagerV2/StudentV1/Entities/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+//Java java.util	com.mycompany.myproject.mymodule
+namespace StudentV1.Entities
+{
+	public class Student
+	{//  class chính là cái Khuôn bên trong chứa các place holder ứng với
+	 //  các đặc điểm của mỗi object thuộc về Class/Nhóm này
+	 //Để có đc 1 sản phẩ từ Khuôn clone ra object, ta cần phải đúc - hành động đổ
+	 //vật liệu vào trong Khuôn
+		private string _id;		//place holder _____
+        private string _name;	//quy ước dùng cú pháp con lạc đà
+        private string _email;	//và _ ở đầu từ _basicSalary
+        private int _yob;
+        private double _gpa;
+
+        public Student(string id, string name, string email, int yob, double gpa)
+        {
+            _id = id;   // ko cần this. vì ko  có sự nhầm lẫn về tên gọi
+            _name = name;
+            _email = email;
+            _yob = yob;
+            _gpa = gpa;
+        }
+
+        // tạo phễu để hứng vật liệu đổ vào Khuôn
+        // cũng giống như tạo hàm để hứng data đưa vào place holder
+        // phễu dùng để hứng vật liệu đổ vào trong khuôn tạo object, PHỄU/CREATE/CONSTRUCT OBJECT
+        // HÀM DÙNG ĐỂ HỨNG DATA BÊN NGOÀI ĐỔ VÀO ĐẶC ĐIỂM CỦA OBJECT, ĐỔ VÀO CÁC PLACE HOLDER
+        // HÀM NÀY GỌI LÀ CONSTRUCTOR DÙNG ĐỂ CONSTRUCT/CREATE/CLONE OBJECT
+
+        // mặc định info của ta là private cho sự riêng tư
+        //khi cần giao tiếp ta  mới bung ra, khoe vừa đủ, hay khoe hết
+        // hành động giao tiếp để thấy được info của nhau, lấy info của nhau, gọi là Get()
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public int GetYob()
+        {
+            return _yob;
+        }   //traditional - truyền thống
+
+        public double GetGpa() => _gpa; //body expression
+        public string GetEmail() => _email;
+        public string GetId() => _id;
+
+        //hành động show hết info, show hết profile, show những thứ internal 1 lượt => ToString()
+        //biến đổi mọi info đang có thành chuỗi dài show ra
+        //1 phiên bản trả về chuỗi, 1 phiên bản in ra all
+
+        public void ShowProfile()
+        {
+            Console.WriteLine(@$"Here is my profile:
+Id: {_id}
+Name: {_name}
+Yob: {_yob}
+Gpa: {_gpa}
+Email: {_email}");
+        }   //verbatim interpolation
+    }
+}
+
---
+++ Session03-OOP/SchoolManagerV2/StudentV1/Program.cs
@@ -0,0 +1,60 @@
+﻿using StudentV1.Entities;
+
+namespace StudentV1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        CreateAStudentObjectV4();
+        Console.ReadLine();
+    }
+
+    static void CreateAStudentObjectV4()
+    {
+        // style new kiểu mới!!!
+        Student nhat = new (gpa: 8.8, yob: 2002, name: "NhatMM", id: "SE1", email: "nhatmm@fpt.edu.vn" );
+
+        nhat.ShowProfile();
+    }//chủ động dưa value vào hàm lộn xộn thứ tự, miễn là đi kèm tên biến máy runtime biết để map value cho đúng!!!
+    //NAMED-ARGUMENT, ĐƯA VALUE KÈM TÊN THAM SỐ
+
+    static void CreateAStudentObjectV3()
+    {
+        // style new kiểu mới nữa!!!
+        // bỏ kiểu dữ liệu sau new vì đã có data type
+
+        Student nhat = new("SE1", "NhatMM", "nhatmm@fpt.edu.vn", 2002, 8.8);
+
+        nhat.ShowProfile();
+    }//tự suy luận nhat là Student do value gán vào
+
+    static void CreateAStudentObjectV2()
+    {
+        // style new kiểu mới!!!
+        var nhat = new Student("SE1", "NhatMM", "nhatmm@fpt.edu.vn", 2002, 8.8);
+       
+        nhat.ShowProfile();
+    }//tự suy luận nhat là Student do value gán vào
+
+    static void CreateAStudentObjectV1()
+    {
+        // tạo mới 1 obj sv, 1 sv cụ thể, 1 thing complex, style truyền thống
+        // con người luôn có xu hướng đặt tên cho mọi thứ quanh họ, sau khi đã mô tả chúng!!
+        // name: Leonardo... nick: Messi, goals: 800
+        // ta gọi obj này là Messi!
+        // 3.14 -> PI, 300.000km/s -> c
+        // 9.8 m/s -> g
+        // 10%, 8% -> VAT
+        //s = v * t
+
+        //ta sẽ gọi 1 bạn sv nào đó = nick name gọn gàng kiểu như
+        // nhat = { id = SE1, name: NhatMM, yob: 2002... }
+        Student nhat = new Student("SE1", "NhatMM", "nhatmm@fpt.edu.vn", 2002, 8.8);
+        //nhat -> biến obj
+        //new Student(...) là 1 thing cụ thể, thứ phức tạp
+        // OBJECT
+
+        nhat.ShowProfile();
+    }
+}
+
