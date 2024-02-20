5e91da0366334df7910ab745a8c1074bd235713e
minatisleeping <minatt2002@gmail.com>
2024-02-20 11:32:12 +0700

    Update project

---
+++ YearEndSchoolManager/Repositories/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Repositories
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ YearEndSchoolManager/Services/Cabinet.cs
@@ -0,0 +1,19 @@
+﻿using Repositories.Entities;
+
+namespace Services
+{
+    public class Cabinet<T> //Type - data type as a parameter - GENERIC
+        //Java: List<Student> list = new ArrayList<Studen>();
+        //Cabinet<Student> seBox = new Cabinet();
+        //Cabinet<Lecturer> seBox = new Cabinet();
+        //
+    {
+        //private Student[] _list1 = new Student[300];
+        //private Lecturer[] _list2 = new Lecturer[300];
+        //private int _count1 = 0;
+        //private int _count2 = 0;
+
+        private T[] _list1 = new T[300];
+
+    }
+}
