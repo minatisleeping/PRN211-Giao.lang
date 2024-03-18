c20d61af18d25d8dd3f4cae07c2cf4997df93ddd
minatisleeping <minatt2002@gmail.com>
2024-03-18 11:37:40 +0700

    connect db duoc roi ehhee bantumlum

---
+++ Practical Exam/PE_PRN211_SP24_PracticalTest_HoangNgocTrinh/Repositories/HoangNgocTrinhContext.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Repositories.Entities;
+
+namespace Repositories;
+
+public partial class HoangNgocTrinhContext : DbContext
+{
+    public HoangNgocTrinhContext()
+    {
+    }
+
+    public HoangNgocTrinhContext(DbContextOptions<HoangNgocTrinhContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    public virtual DbSet<BookCategory> BookCategories { get; set; }
+
+    public virtual DbSet<UserAccount> UserAccounts { get; set; }
+
+    //hàm đọc chuỗi kết nối CSDL từ file json: appsettings.json
+    //private vì chỉ cần dùng nội bộ class này
+    private string? GetConnectionString()
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true).Build();
+        return configuration["ConnectionStrings:DBDefault"];
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(GetConnectionString());
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.BookId).HasName("PK__Book__3DE0C20756EC3F03");
+
+            entity.ToTable("Book");
+
+            entity.Property(e => e.BookId).ValueGeneratedNever();
+            entity.Property(e => e.Author).HasMaxLength(50);
+            entity.Property(e => e.BookName).HasMaxLength(100);
+            entity.Property(e => e.Description).HasMaxLength(1000);
+            entity.Property(e => e.PublicationDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.BookCategory).WithMany(p => p.Books)
+                .HasForeignKey(d => d.BookCategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Book_BookCategory");
+        });
+
+        modelBuilder.Entity<BookCategory>(entity =>
+        {
+            entity.HasKey(e => e.BookCategoryId).HasName("PK__BookCate__6347EC047D82D06A");
+
+            entity.ToTable("BookCategory");
+
+            entity.Property(e => e.BookCategoryId).ValueGeneratedNever();
+            entity.Property(e => e.BookGenreType).HasMaxLength(50);
+            entity.Property(e => e.Description).HasMaxLength(500);
+        });
+
+        modelBuilder.Entity<UserAccount>(entity =>
+        {
+            entity.HasKey(e => e.MemberId).HasName("PK__UserAcco__0CF04B18CD5360B0");
+
+            entity.ToTable("UserAccount");
+
+            entity.Property(e => e.MemberId).ValueGeneratedNever();
+            entity.Property(e => e.Email).HasMaxLength(50);
+            entity.Property(e => e.FullName).HasMaxLength(50);
+            entity.Property(e => e.Password).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
